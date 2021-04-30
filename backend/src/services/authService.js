34d0d1f71984130e8e69c7c73c32da1ad25eb72a
Chai Pin Zheng <pinzheng61.20@ichat.sp.edu.sg>
2021-04-30 21:48:27 +0800

    Formatting

--- backend/src/services/authService.js
+++ backend/src/services/authService.js
@@ -1,30 +1,24 @@
 config = require('../config/config');
 const pool = require('../config/database')
 module.exports.authenticateUser = (email, callback) => {
-
     pool.getConnection((err, connection) => {
         if (err) {
             if (err) throw err;
-
         } else {
             try {
                 connection.query(`SELECT user.user_id, first_name, last_name, status, email, user_password, role_name, user.role_id  
                    FROM user INNER JOIN role ON user.role_id=role.role_id AND email='${email}'`, {}, (err, rows) => {
                     if (err) {
                         if (err) return callback(err, null);
-
                     } else {
                         if (rows.length == 1) {
                             console.log(rows);
                             return callback(null, rows);
-
                         } else {
-
                             return callback('Login has failed', null);
                         }
                     }
                     connection.release();
-
                 });
             } catch (error) {
                 return callback(error, null);;
@@ -34,7 +28,6 @@ module.exports.authenticateUser = (email, callback) => {
 
 } // End of authenticate
 module.exports.getOneUserStatusData = async (userEmail) => {
-
     return new Promise((resolve, reject) => {
         pool.getConnection(async (err, connection) => {
             if (err) {
@@ -53,45 +46,36 @@ module.exports.getOneUserStatusData = async (userEmail) => {
                         console.log(rows);
                         resolve(rows[0]);
                     }
-
                 });
             };
         });
-
-
     }); //End of new Promise object creation
-
 } // End of getOneUserStatusData
-module.exports.authenticateAdmin = (email, callback) => {
 
+
+module.exports.authenticateAdmin = (email, callback) => {
     pool.getConnection((err, connection) => {
         if (err) {
             if (err) throw err;
-
         } else {
             try {
                 connection.query(`SELECT user.user_id, first_name, last_name, email, user_password, role_name, user.role_id  
                    FROM user INNER JOIN role ON user.role_id=role.role_id AND role_name='admin' AND email=?`, [email], (err, rows) => {
                     if (err) {
                         if (err) return callback(err, null);
-
                     } else {
                         if (rows.length == 1) {
                             console.log(rows);
                             return callback(null, rows);
-
                         } else {
-
                             return callback('Login has failed', null);
                         }
                     }
                     connection.release();
-
                 });
             } catch (error) {
                 return callback(error, null);;
             }
         }
     }); // End of getConnection
-
 } // End of authenticateAdmin
