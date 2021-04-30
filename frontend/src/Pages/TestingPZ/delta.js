4c0db9000c12a9fa891836a00ed84c72ec80b6e7
Chai Pin Zheng <pinzheng61.20@ichat.sp.edu.sg>
2021-05-01 02:40:02 +0800

    OTP Function Version

--- frontend/src/Pages/TestingPZ/delta.js
+++ frontend/src/Pages/TestingPZ/delta.js
@@ -3,62 +3,48 @@ import { render } from 'react-dom';
 import './delta.css'
 import OtpInput from 'react-otp-input';
 
-export default class Demo extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            otp: '',
-        };
-    }
+export default function OneTimePassword(props) {
+    var [otp, setOtp] = useState();
 
-    handleOtpChange = otp => {
+    const handleOtpChange = (otp) => {
         this.setState({ otp });
     };
 
-    handleSubmit = e => {
+    const handleSubmit = (e) => {
         e.preventDefault();
         alert(this.state.otp);
     };
 
-    render() {
-        const { otp, numInputs } = this.state;
-
-        return (
-            <div className="container">
-                <div className="view">
-                    <div className="card">
-                        <form onSubmit={this.handleSubmit}>
-                            <h2>Enter verification code</h2>
-                            <div className="margin-top--small">
-                                <OtpInput
-                                    inputStyle={{
-                                        width: '3rem',
-                                        height: '3rem',
-                                        margin: '0 1rem',
-                                        fontSize: '2rem',
-                                        borderRadius: 4,
-                                        border: '1px solid rgba(0,0,0,0.3)',
-                                    }}
-                                    numInputs={6}
-                                    isDisabled={false}
-                                    hasErrored={false}
-                                    errorStyle="error"
-                                    onChange={this.handleOtpChange}
-                                    separator={<span>-</span>}
-                                    isInputNum={true}
-                                    shouldAutoFocus
-                                />
-                            </div>
-                            <button
-                                className="btn margin-top--large"
-                                disabled={otp.length < numInputs}
-                            >
-                                Get OTP
-              </button>
-                        </form>
-                    </div>
+    return (
+        <div className="container">
+            <div className="view">
+                <div className="card">
+                    <form onSubmit={this.handleSubmit}>
+                        <h2>Enter verification code</h2>
+                        <div className="margin-top--small">
+                            <OtpInput
+                                inputStyle={{
+                                    width: '3rem',
+                                    height: '3rem',
+                                    margin: '0 1rem',
+                                    fontSize: '2rem',
+                                    borderRadius: 4,
+                                    border: '1px solid rgba(0,0,0,0.3)',
+                                }}
+                                numInputs={6}
+                                isDisabled={false}
+                                hasErrored={false}
+                                errorStyle="error"
+                                onChange={this.handleOtpChange}
+                                separator={<span>-</span>}
+                                isInputNum={true}
+                                shouldAutoFocus
+                            />
+                        </div>
+                        <button className="btn margin-top--large" disabled={6 < 6}>Verify OTP</button>
+                    </form>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
