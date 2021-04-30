d71d9466a3e8de8aaf09e9b27b8305b734629021
Chai Pin Zheng <pinzheng61.20@ichat.sp.edu.sg>
2021-04-30 22:32:57 +0800

    OTP Password

--- backend/src/controllers/pb4.js
+++ backend/src/controllers/pb4.js
@@ -1,6 +1,8 @@
 const config = require('../config/config');
 const mailgun = require("mailgun-js");
 
+const sleep = require('sleep-promise');
+
 //Task 04 - Extracts the user params via the params. Afterwards, uses mailguns as well as sendgrid to send a email to the user
 //Success Response
 //Code: 204 No Content
@@ -9,13 +11,18 @@ exports.processUserEmailOTP = async (req, res, next) => {
     const userEmail = req.params.userEmail;
     const mg = mailgun({ apiKey: config.mailGunApiKey, domain: config.mailGunDomain });
 
+    code = "OBQEZE"
     try {
-        await sleep(7000);
+        await sleep(3500);
         let emailData = {
-            from: `competition system admin <admin@samples.mailgun.org>`,
-            to: `{${userEmail}}}`,
-            subject: `Your one time password is ... ${123456}`,
-            text: `Thank you ${data[index].firstName} ${data[index].lastName} for registering as participant for the competition. Please submit your business plans before the deadline.`
+            from: `Competition System Admin <support@sp.competitionmanagementsystem.org>`,
+            to: `chaipinzheng@gmail.com`,
+            subject: `Competition System Verification Code`,
+            text: `You are currently submitting a form on FormSG.\n
+            \nYour OTP is ${code}. It will expire in 5 minutes. Please use this to verify your submission.
+            \nIf your OTP does not work, please request for a new OTP.
+            \nIf you did not make this request, you may ignore this email.
+            \n\nThe Competition Management System Support Team`
         };
 
         mg.messages().send(emailData, function (error, body) {
