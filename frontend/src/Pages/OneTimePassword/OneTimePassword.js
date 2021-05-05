8fd5d739895b67dbe79f555d1a1d562164440353
Chai Pin Zheng <pinzheng61.20@ichat.sp.edu.sg>
2021-05-05 11:28:47 +0800

    COmmit

--- frontend/src/Pages/OneTimePassword/OneTimePassword.js
+++ frontend/src/Pages/OneTimePassword/OneTimePassword.js
@@ -90,6 +90,7 @@ export default function OneTimePassword(props) {
             });
     }
 
+<<<<<<< HEAD
     // for request new OTP
     const requestNewOTP = () => {
         setLoading(true);
@@ -160,6 +161,8 @@ export default function OneTimePassword(props) {
                 }
             });
     };
+=======
+>>>>>>> parent of 2863d1f (Resending of OTPs!)
     const redirect = () => {
         history.push('/register');
     }
